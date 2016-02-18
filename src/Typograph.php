f900c2d4712b3385ec110b70db153d870a8271a3
Denis Beliaev <cimmwolf@gmail.com>
2016-02-18 11:43:38 +0700

    Init commit

---
+++ src/Typograph.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Author: Denis Beliaev <cimmwolf@gmail.com>
+ */
+
+namespace DenisBeliaev;
+require_once __DIR__ . '../vendor/emuravjev/mdash/src-php/EMT.php';
+
+class Typograph
+{
+    /**
+     * @param string $path Absolute path to filename
+     * @param array $settings Settings for emuravjev/mdash
+     * @return string
+     */
+    static function file($path, $settings) {
+        ob_start();
+        include $path;
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return self::string($content, $settings);
+    }
+
+    /**
+     * @param string $content Content for typograph
+     * @param array $settings  Settings for emuravjev/mdash
+     * @return string
+     */
+    static function string($content, $settings = [])
+    {
+        $settings = array_merge($settings, [
+            'Text.auto_links' => 'off',
+            'Text.email' => 'off',
+            'Text.paragraphs' => 'off',
+            'Text.breakline' => 'off',
+        ]);
+
+
+        $T = new \EMTypograph();
+        $T->setup($settings);
+        $T->set_text($content);
+
+        return $T->apply();
+    }
+}
