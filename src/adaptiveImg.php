035c1e41e3db148c60a44bdae1501cd1c4c164ab
Denis Deliaev <cimmwolf@gmail.com>
2016-04-09 14:58:10 +0800

    Add new version of AdaptiveImg class

--- src/adaptiveImg.php
+++ src/adaptiveImg.php
@@ -6,51 +6,60 @@
 namespace DenisBeliaev;
 
 /**
- * Class Img
- * @package DenisBeliaev
- * @deprecated
+ * Class adaptiveImg
+ * @package Utilites
  */
-class Img
+class adaptiveImg
 {
+    public $root;
+
+    /**
+     * adaptiveImg constructor.
+     * @param string $root path to project's root
+     */
+    public function __construct($root = '')
+    {
+        if (empty($root))
+            $root = realpath(__DIR__ . '/../../../..');
+        $this->root = $root;
+    }
+
     /** Create img tag with parameters srcset (type w) and sizes
      *
-     * @deprecated 
-     * 
-     * @param array $source [src, *path to file* or *maximum width of image*]
+     * @param array $src
      * @param string $alt alt attribute
      * @param string $sizes sizes attribute
      * @param array $widths widths of image
      * @param array $htmlOptions other attributes of the img tag in attribute => value format
-     *
-     * @return string HTML img tag 
-     * 
+     * @param $filePath
+     * @param int $maxWidth
+     * @return string HTML img tag
      */
-    static function w($source, $alt, $sizes, $widths, $htmlOptions = [])
+    public function typeW($src, $alt, $sizes, $widths, $htmlOptions = [], $filePath = '', $maxWidth = 1920)
     {
         $srcSet = [];
-        if(is_int($source[1]))
-            $realWidth = $source[1];
-        else {
-            $imgInfo = @getimagesize(__DIR__ . trim($source[1]));
-            $realWidth = 1920;
-            if ($imgInfo !== false)
-                $realWidth = $imgInfo[0];
-        }
-        $src = $source[0];
+
+        if (empty($filePath) AND $src[0] == '/')
+            $filePath = realpath($this->root . $src);
+
+        $imgInfo = @getimagesize($filePath);
+
+        if ($imgInfo !== false)
+            $maxWidth = $imgInfo[0];
 
         foreach ($widths as $width) {
             $widths[] = $width * 2;
             $widths[] = $width * 3;
         }
 
-        if (!in_array($realWidth, $widths))
-            $widths[] = $realWidth;
+        if (!in_array($maxWidth, $widths))
+            $widths[] = $maxWidth;
 
         $widths = array_unique($widths);
         sort($widths);
 
         foreach ($widths as $width)
-            if ($width <= $realWidth)
+            if ($width <= $maxWidth)
                 $srcSet[] = preg_replace('#^(.*?)\.(jpe?g|png|gif)$#', "$1@{$width}x-.$2 {$width}w", $src);
         $htmlOptions['srcset'] = implode(', ', $srcSet);
         $htmlOptions['sizes'] = $sizes;
